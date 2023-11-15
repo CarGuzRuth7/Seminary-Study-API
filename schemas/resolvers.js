8ee5be6b78dd833b170f875fdaaa4c5994f48727
CarGuzRuth7 <cardonaguzmanruth97@gmail.com>
2023-11-15 16:24:31 -0300

    Integrate apollo server to project

---
+++ schemas/resolvers.js
@@ -0,0 +1,10 @@
+//graphql resolvers functions
+
+const resolvers = {
+    Query: {
+        hello: ()=>{ return "hello work"}
+    }
+}
+
+
+module.exports = resolvers
---
+++ schemas/typeDefs.js
@@ -0,0 +1,10 @@
+//Type Definition Schemas 
+
+const typeDefs = `#graphql
+    type Query {
+     hello: String
+    
+    }
+`;
+
+module.exports = typeDefs
