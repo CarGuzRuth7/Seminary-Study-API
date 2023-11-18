0e20caaa660f497d1f81f257a283ba674a307a10
sara latorre <saralatorre97@gmail.com>
2023-11-18 23:03:32 +0100

    updated quotes resolver

--- resolvers/quotes.js
+++ resolvers/quotes.js
@@ -1,26 +1,21 @@
 const quotesResolvers = {
   Query: {
-    getQuotes: async (_, __, context) => {
-      // not implemented yet
-      return context.Quote.getAllQuotes();
+    getQuotes: async () => {
+      return "getQuotes resolver is not implemented yet";
     },
-    getQuote: async (_, { id }, context) => {
-      // not implemented yet
-      return context.Quote.getQuoteById(id);
+    getQuote: async (_, { id }) => {
+      return `getQuote resolver for ID ${id} is not implemented yet`;
     }
   },
   Mutation: {
-    createQuote: async (_, { content, author }, context) => {
-      // not implemented yet
-      return context.Quote.createQuote({ content, author });
+    createQuote: async (_, { content, author }) => {
+      return `createQuote resolver for ${content} by ${author} is not implemented yet`;
     },
-    updateQuote: async (_, { id, content, author }, context) => {
-      // not implemented yet
-      return context.Quote.updateQuoteById({ id, content, author });
+    updateQuote: async (_, { id, content, author }) => {
+      return `updateQuote resolver for ID ${id} with ${content} by ${author} is not implemented yet`;
     },
-    deleteQuote: async (_, { id }, context) => {
-      // not implemented yet
-      return context.Quote.deleteQuoteById(id);
+    deleteQuote: async (_, { id }) => {
+      return `deleteQuote resolver for ID ${id} is not implemented yet`;
     }
   }
 };
