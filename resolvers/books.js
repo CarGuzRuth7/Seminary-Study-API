89ce430b734c1fb7327534c4bb7b84995e83c1a9
sara latorre <saralatorre97@gmail.com>
2023-11-18 23:37:12 +0100

    formatted

--- resolvers/books.js
+++ resolvers/books.js
@@ -8,7 +8,6 @@ const booksResolvers = {
       const books = [
         { book: "1 Nephi", chapter: 22, verse: 31, content: "Content for 1 Nephi" },
         { book: "2 Nephi", chapter: 3, verse: 15, content: "Content for 2 Nephi" }
-        // Add other books
       ];
       return books;
     }
