ac317d30e8c94b52e098f4739c0336ec8dd888cf
sara latorre <saralatorre97@gmail.com>
2023-11-28 14:30:35 +0100

    added scriptures resolvers

---
+++ schemas/scriptures.js
@@ -0,0 +1,36 @@
+const { gql } = require("apollo-server-express");
+
+const scripturesDefs = gql`
+  """
+  Represents a Scripture to master
+  """
+  type Scripture {
+    book: String!
+    chapter: Int!
+    verses: [String]!
+    scripture: String!
+    theme: String!
+  }
+
+  """
+  Queries related to scriptures.
+  """
+  type Query {
+    """
+    Get all the scriptures to master present in the database
+    """
+    getScriptures: [Scripture]!
+  }
+
+  """
+  Mutations related to scriptures.
+  """
+  type Mutation {
+    """
+    Get scriptures by providing search input
+    """
+    searchScriptures(searchTerm: String): [Scripture]!
+  }
+`;
+
+module.exports = scripturesDefs;
